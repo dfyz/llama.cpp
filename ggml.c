92541002b0516dad6f9f256a1f814cdf57cd94ec
Ivan Komarov <Ivan.Komarov@dfyz.info>
2023-04-03 04:30:19 +0200

    second try

--- ggml.c
+++ ggml.c
@@ -497,6 +497,21 @@ static inline __m128i packNibbles( __m128i bytes1, __m128i bytes2 )
 }
 #endif
 
+inline static __m512i blk_2_bytes(__m512i x) {
+    const __m512i prm = _mm512_set_epi8(
+        39, 39, 38, 38, 37, 37, 36, 36, 35, 35, 34, 34, 33, 33, 32, 32,
+        31, 31, 30, 30, 29, 29, 28, 28, 27, 27, 26, 26, 25, 25, 24, 24,
+        19, 19, 18, 18, 17, 17, 16, 16, 15, 15, 14, 14, 13, 13, 12, 12,
+        11, 11, 10, 10, 9, 9, 8, 8, 7, 7, 6, 6, 5, 5, 4, 4
+    );
+    const __m512i y = _mm512_permutexvar_epi8(prm, x);
+
+    const __m512i prm2 = _mm512_set1_epi64(0x3c302c201c100c00);
+    const __m512i z = _mm512_multishift_epi64_epi8(prm2, y);
+
+    return _mm512_and_si512(_mm512_set1_epi8(0xF), z);
+}
+
 // method 5
 // blocks of QK elements
 // represented with a single float (delta) and QK/2 8-bit ints (i.e QK 4-bit signed integer factors)
@@ -1932,32 +1947,48 @@ static void ggml_vec_dot_q4_0(const int n, float * restrict s, const void * rest
     sumf = sum0 + sum1;
 #elif defined(__AVX512F__)
     // Initialize accumulator with zeros
-    __m512 acc0 = _mm512_setzero_ps();
-    __m512 acc1 = _mm512_setzero_ps();
+    __m512 acc = _mm512_setzero_ps();
 
-    const int superblock_size = 8;
-    const int superblock_count = nb / superblock_size;
+    const __m512i off = _mm512_set1_epi8(8);
+    const __mmask16 scale_mul_mask = 0x21;
 
-    for (int superblock_ix = 0; superblock_ix < superblock_count; superblock_ix += 1) {
-        int i = superblock_ix * superblock_size;
+    const __m512i prm = _mm512_set_epi32(
+        5, 5, 5, 5,
+        5, 5, 5, 5,
+        0, 0, 0, 0,
+        0, 0, 0, 0
+    );
 
-        acc0 = dot_q4_0_oneblock_avx512( acc0, x, y, i+0 );
-        acc1 = dot_q4_0_oneblock_avx512( acc1, x, y, i+1 );
-        acc0 = dot_q4_0_oneblock_avx512( acc0, x, y, i+2 );
-        acc1 = dot_q4_0_oneblock_avx512( acc1, x, y, i+3 );
-        acc0 = dot_q4_0_oneblock_avx512( acc0, x, y, i+4 );
-        acc1 = dot_q4_0_oneblock_avx512( acc1, x, y, i+5 );
-        acc0 = dot_q4_0_oneblock_avx512( acc0, x, y, i+6 );
-        acc1 = dot_q4_0_oneblock_avx512( acc1, x, y, i+7 );
-    }
+    for (int i = 0; i < nb; i += 2) {
+        __m512i blk0 = _mm512_loadu_si512(&x[i]);
+        __m512i blk1 = _mm512_loadu_si512(&y[i]);
 
-    // Remainders
-    for (int i = superblock_count * superblock_size; i < nb; ++i) {
-        acc0 = dot_q4_0_oneblock_avx512( acc0, x, y, i );
+        __m512 scales = _mm512_maskz_mul_ps(scale_mul_mask, (__m512)blk0, (__m512)blk1);
+        scales = _mm512_permutexvar_ps(prm, scales);
+
+        const __m512i bx = blk_2_bytes(blk0);
+        __m512i by = blk_2_bytes(blk1);
+        by = _mm512_sub_epi8(by, off);
+
+        const __m512i aa = _mm512_dpbusds_epi32(
+            _mm512_setzero_epi32(),
+            bx, by
+        );
+        const __m512i bb = _mm512_dpbusds_epi32(
+            _mm512_setzero_epi32(),
+            off, by
+        );
+        const __m512i diff = _mm512_sub_epi32(aa, bb);
+
+        acc = _mm512_fmadd_ps(
+            scales,
+            _mm512_cvtepi32_ps(diff),
+            acc
+        );
     }
 
     // Horizontal sum of all lanes of the accumulator
-    sumf = _mm512_reduce_add_ps( acc0 ) + _mm512_reduce_add_ps( acc1 );
+    sumf = _mm512_reduce_add_ps( acc );
 #elif defined(__AVX2__)
     // Initialize accumulator with zeros
     __m256 acc = _mm256_setzero_ps();
