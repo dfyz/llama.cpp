99c728f6aff5b5aae37439ff40c32e0889f4eb98
Ivan Komarov <Ivan.Komarov@dfyz.info>
2023-04-08 04:09:50 +0200

    cleanup

--- ggml.c
+++ ggml.c
@@ -93,6 +93,13 @@ typedef void* thread_ret_t;
 #endif
 #endif
 
+#define GGML_MODERN_AVX512_SUPPORT 0
+
+#if __AVX512F__ && __AVX512VBMI__ && __AVX512VNNI__
+    #undef GGML_MODERN_AVX512_SUPPORT
+    #define GGML_MODERN_AVX512_SUPPORT 1
+#endif
+
 #ifdef __HAIKU__
 #define static_assert(cond, msg) _Static_assert(cond, msg)
 #endif
@@ -1758,6 +1765,54 @@ inline static void ggml_vec_dot_f32(const int n, float * restrict s, const float
     *s = sumf;
 }
 
+#if GGML_MODERN_AVX512_SUPPORT
+static inline __m512i bytes_from_q4_0_twoblocks_avx512( const __m512i blocks ) {
+    const __m512i byte_perm = _mm512_set_epi8(
+        39, 38, 39, 38, 37, 36, 37, 36, 35, 34, 35, 34, 33, 32, 33, 32,
+        31, 30, 31, 30, 29, 28, 29, 28, 27, 26, 27, 26, 25, 24, 25, 24,
+        19, 18, 19, 18, 17, 16, 17, 16, 15, 14, 15, 14, 13, 12, 13, 12,
+        11, 10, 11, 10,  9,  8,  9,  8,  7,  6,  7,  6,  5,  4,  5,  4
+    );
+    const __m512i permuted = _mm512_permutexvar_epi8( byte_perm, blocks );
+
+    const __mmask32 shift_mask = 0xaaaaaaaa;
+    const __m512i shifted = _mm512_mask_srai_epi16( permuted, shift_mask, permuted, 4 );
+
+    const __m512i low_nibble_mask = _mm512_set1_epi8( 0xf );
+    return _mm512_and_si512( low_nibble_mask, shifted );
+}
+
+static inline __m512 dot_q4_0_twoblocks_avx512(
+    __m512 acc,
+    const block_q4_0 * restrict x,
+    const block_q4_0 * restrict y,
+    int i
+) {
+    const __m512i blocks_0 = _mm512_loadu_si512( &x[i] );
+    const __m512i blocks_1 = _mm512_loadu_si512( &y[i] );
+
+    const __m512 scales = _mm512_maskz_mul_ps( 0x21, (__m512)blocks_0, (__m512)blocks_1 );
+    const __m512i scale_perm = _mm512_set_epi32(
+        5, 5, 5, 5, 5, 5, 5, 5,
+        0, 0, 0, 0, 0, 0, 0, 0
+    );
+    const __m512 permuted_scales = _mm512_permutexvar_ps( scale_perm, scales );
+
+    const __m512i bytes_0 = bytes_from_q4_0_twoblocks_avx512( blocks_0 );
+    const __m512i bytes_1 = bytes_from_q4_0_twoblocks_avx512( blocks_1 );
+    const __m512i offset_8 = _mm512_set1_epi8( 8 );
+    const __m512i bytes_1_with_offset = _mm512_sub_epi8( bytes_1, offset_8 );
+
+    const __m512i zero = _mm512_setzero_epi32();
+    const __m512i prod_0 = _mm512_dpbusds_epi32( zero, bytes_0, bytes_1_with_offset );
+    const __m512i prod_1 = _mm512_dpbusds_epi32( zero, offset_8, bytes_1_with_offset );
+    const __m512i diff_int = _mm512_sub_epi32( prod_0, prod_1 );
+    const __m512 diff_float = _mm512_cvtepi32_ps( diff_int );
+
+    return _mm512_fmadd_ps( permuted_scales, diff_float, acc );
+}
+#endif
+
 #if __AVX512F__ && QK == 32
 static inline __m512 dot_q4_0_oneblock_avx512(
     __m512 acc,
@@ -1787,68 +1842,6 @@ static inline __m512 dot_q4_0_oneblock_avx512(
     // Apply the scale, and accumulate
     return _mm512_fmadd_ps( d, p, acc );
 }
-
-inline static __m512 blk_2_scales(__m512i x) {
-    const __m512i prm = _mm512_set_epi32(
-        5, 5, 5, 5,
-        5, 5, 5, 5,
-        0, 0, 0, 0,
-        0, 0, 0, 0
-    );
-    return _mm512_permutexvar_ps(prm, (__m512)x);
-}
-
-inline static __m512i blk_2_bytes(__m512i x) {
-    const __m512i prm = _mm512_set_epi8(
-        39, 39, 38, 38, 37, 37, 36, 36, 35, 35, 34, 34, 33, 33, 32, 32,
-        31, 31, 30, 30, 29, 29, 28, 28, 27, 27, 26, 26, 25, 25, 24, 24,
-        19, 19, 18, 18, 17, 17, 16, 16, 15, 15, 14, 14, 13, 13, 12, 12,
-        11, 11, 10, 10, 9, 9, 8, 8, 7, 7, 6, 6, 5, 5, 4, 4
-    );
-    const __m512i y = _mm512_permutexvar_epi8(prm, x);
-
-    const __m512i prm2 = _mm512_set1_epi64(0x3c302c201c100c00);
-    const __m512i z = _mm512_multishift_epi64_epi8(prm2, y);
-
-    return _mm512_and_si512(_mm512_set1_epi8(0xF), z);
-}
-
-static inline __m512 dot_q4_0_twoblocks_avx512(
-    __m512 acc,
-    const block_q4_0 * restrict x,
-    const block_q4_0 * restrict y,
-    int i
-) {
-    __m512i blk0 = _mm512_loadu_si512(&x[i]);
-    __m512i blk1 = _mm512_loadu_si512(&y[i]);
-
-    const __m512 scales = _mm512_mul_ps(
-        blk_2_scales(blk0),
-        blk_2_scales(blk1)
-    );
-
-    const __m512i bx = blk_2_bytes(blk0);
-    __m512i by = blk_2_bytes(blk1);
-    const __m512i off = _mm512_set1_epi8(8);
-    by = _mm512_sub_epi8(by, off);
-
-    const __m512i aa = _mm512_dpbusds_epi32(
-        _mm512_setzero_epi32(),
-        bx, by
-    );
-    const __m512i bb = _mm512_dpbusds_epi32(
-        _mm512_setzero_epi32(),
-        off, by
-    );
-    const __m512i diff = _mm512_sub_epi32(aa, bb);
-
-    return _mm512_fmadd_ps(
-        scales,
-        _mm512_cvtepi32_ps(diff),
-        acc
-    );
-}
-
 #endif
 
 inline static void ggml_vec_dot_f16(const int n, float * restrict s, ggml_fp16_t * restrict x, ggml_fp16_t * restrict y) {
@@ -1997,12 +1990,18 @@ static void ggml_vec_dot_q4_0(const int n, float * restrict s, const void * rest
     __m512 acc0 = _mm512_setzero_ps();
     __m512 acc1 = _mm512_setzero_ps();
 
+#if GGML_MODERN_AVX512_SUPPORT
     const int superblock_size = 16;
+#else
+    const int superblock_size = 8;
+#endif
+
     const int superblock_count = nb / superblock_size;
 
-    for (int superblock_ix = 0; superblock_ix < superblock_count; ++superblock_ix) {
+    for (int superblock_ix = 0; superblock_ix < superblock_count; superblock_ix += 1) {
         int i = superblock_ix * superblock_size;
 
+#if GGML_MODERN_AVX512_SUPPORT
         acc0 = dot_q4_0_twoblocks_avx512( acc0, x, y, i+0 );
         acc1 = dot_q4_0_twoblocks_avx512( acc1, x, y, i+2 );
         acc0 = dot_q4_0_twoblocks_avx512( acc0, x, y, i+4 );
@@ -2011,12 +2010,28 @@ static void ggml_vec_dot_q4_0(const int n, float * restrict s, const void * rest
         acc1 = dot_q4_0_twoblocks_avx512( acc1, x, y, i+10 );
         acc0 = dot_q4_0_twoblocks_avx512( acc0, x, y, i+12 );
         acc1 = dot_q4_0_twoblocks_avx512( acc1, x, y, i+14 );
+#else
+        acc0 = dot_q4_0_oneblock_avx512( acc0, x, y, i+0 );
+        acc1 = dot_q4_0_oneblock_avx512( acc1, x, y, i+1 );
+        acc0 = dot_q4_0_oneblock_avx512( acc0, x, y, i+2 );
+        acc1 = dot_q4_0_oneblock_avx512( acc1, x, y, i+3 );
+        acc0 = dot_q4_0_oneblock_avx512( acc0, x, y, i+4 );
+        acc1 = dot_q4_0_oneblock_avx512( acc1, x, y, i+5 );
+        acc0 = dot_q4_0_oneblock_avx512( acc0, x, y, i+6 );
+        acc1 = dot_q4_0_oneblock_avx512( acc1, x, y, i+7 );
+#endif
     }
 
     // Remainders
+#if GGML_MODERN_AVX512_SUPPORT
     for (int i = superblock_count * superblock_size; i < nb; i += 2) {
         acc0 = dot_q4_0_twoblocks_avx512( acc0, x, y, i );
     }
+#else
+    for (int i = superblock_count * superblock_size; i < nb; ++i) {
+        acc0 = dot_q4_0_oneblock_avx512( acc0, x, y, i );
+    }
+#endif
 
     // Horizontal sum of all lanes of the accumulator
     sumf = _mm512_reduce_add_ps( acc0 ) + _mm512_reduce_add_ps( acc1 );
@@ -10723,6 +10738,14 @@ int ggml_cpu_has_avx2(void) {
 
 int ggml_cpu_has_avx512(void) {
 #if defined(__AVX512F__)
+    return 1;
+#else
+    return 0;
+#endif
+}
+
+int ggml_cpu_has_modern_avx512(void) {
+#if GGML_MODERN_AVX512_SUPPORT
     return 1;
 #else
     return 0;
